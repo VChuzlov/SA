0aa6fc5a6bce8a5329c1bf1ea0eadc22604adb96
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2021-03-04 22:43:10 +0700

    123

--- 2021/ЛБ3_кин_парам/project/test/main.pas
+++ 2021/ЛБ3_кин_парам/project/test/main.pas
@@ -12,17 +12,17 @@ end;
 
 
 function runge_kutt(
-         func: function(time:real; c, k: array of real): array of real;
-         c, k: array of real; start, stop: real; 
+         func: function(time: real; c, k: array of real): array of real; 
+         c, k: array of real; start, stop: real;
          h: real := 0.01): array of array of real;
-         
-  function sum_(arr1, arr2: array of real; a: real): array of real;
+
+  function sum(arr1, arr2: array of real; a: real): array of real;
   begin
-    result := ArrFill(arr1.Length, 0.0); 
+    result := ArrFill(arr1.Length, 0.0);
     for var i := 0 to result.High do
-      result[i] += arr1[i] + a * arr2[i]
+      result[i] := arr1[i] + arr2[i] * a
   end;
-  
+
 begin
   var k1 := ArrFill(c.Length, 0.0);
   var k2 := ArrFill(c.Length, 0.0);
@@ -30,13 +30,12 @@ begin
   var k4 := ArrFill(c.Length, 0.0);
   
   var iter := Trunc((stop - start) / h) + 1;
+  var time := start;
+  var c_ := copy(c);
   
   SetLength(result, iter);
   for var i := 0 to result.High do
     SetLength(result[i], c.Length+1);
-  
-  var c_ := copy(c);
-  var time := start;
   
   for var i := 0 to result.High do
   begin
@@ -45,19 +44,20 @@ begin
       result[i][j+1] := c_[j];
     
     k1 := func(time, c_, k);
-    k2 := func(time + h / 2, sum_(c_, k1, h / 2), k);
-    k3 := func(time + h / 2, sum_(c_, k2, h / 2), k);
-    k4 := func(time + h, sum_(c_, k3, h), k);
+    k2 := func(time + h / 2, sum(c_, k1, h / 2), k);
+    k3 := func(time + h / 2, sum(c_, k2, h / 2), k);
+    k4 := func(time + h, sum(c_, k3, h), k);
     
     for var j := 0 to c_.High do
       c_[j] += h / 6 * (k1[j] + 2 * k2[j] + 2 * k3[j] + k4[j]);
     
     time += h
   end;
+  
 end;
 
 
-function obj_func(k, act_values: array of real): real;
+function obj_func(k, act_values:array of real): real;
 begin
   var res := runge_kutt(kin_scheme, |1.0, 0.0, 0.0, 0.0|, k, 0.0, 1.0);
   
@@ -65,23 +65,22 @@ begin
   foreach var i in range(20, res.High, 10) do
     res_ := res_ + res[i][1:];
   
-  for var i := 0 to act_values.High do
+  for var i := 0 to res_.High do
     result += (act_values[i] - res_[i]) ** 2;
 end;
 
 
-begin  
+begin
+//  var res := runge_kutt(kin_scheme, |1.0, 0.0, 0.0, 0.0|, |1.0, 1.0|, 0.0, 1.0);
+//  res.PrintLines
+  
   var act_values: array of real;
-  foreach var (i, item) in ReadLines('act_values.txt').Numerate do
+  foreach var (i, el) in REadLines('act_values.txt').Numerate do
   begin
     SetLength(act_values, i);
-    act_values[i-1] := item.ToReal;
+    act_values[i-1] := el.ToReal;
   end;
   
   var k := genetic_algorithm(||1e-3, 10|, |1e-3, 10||, obj_func, act_values)[0];
-  k.Println;
-  
-  var res := runge_kutt(kin_scheme, |1.0, 0.0, 0.0, 0.0|, k, 0.0, 1.0);
-  foreach var i in range(10, res.High, 10) do
-  res[i].Println;
+  k.println
 end.
