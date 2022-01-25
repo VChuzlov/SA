7f52f786e78651a1ce0a0cc51d16af657b9477c7
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2022-01-25 15:20:36 +0700

    123

--- 2022/очн/ЛБ-01/on_calc.pas
+++ 2022/очн/ЛБ-01/on_calc.pas
@@ -3,10 +3,10 @@
 
 function read_txt(filename: string): array of array of real;
 begin
-  foreach var (i, lines) in ReadLines(filename).Numerate(0) do
+  foreach var (i, line) in ReadLines(filename).Numerate(0) do
   begin
-    SetLength(result, i+1);
-    result[i] := lines.ToReals
+    SetLength(result, i + 1);
+    result[i] := line.ToReals
   end;
 end;
 
@@ -40,17 +40,15 @@ end;
 
 begin
   var data := read_txt('data.txt');
-  var fractions := mix_flows(arr(0.1, 0.1, 0.1, 0.1, 0.2, 0.4), 
-                              data);
-                              
+  var ratio := arr(0.1, 0.1, 0.1, 0.1, 0.1, 0.5);
+  var fractions := mix_flows(ratio, data);
   var ron := get_octane_number(fractions, UConst.RON, UConst.Bi);
   
-  var flow := ArrFill(fractions.Length, 0.0);
-  foreach var j in data[0].Indices do
+  var flow := ArrFill(data.Length, 0.0);
+  foreach var j in ratio.Indices do
   begin
-    foreach var i in fractions.Indices do
+    foreach var i in data.Indices do
       flow[i] := data[i][j];
-    
-    get_octane_number(flow, UCOnst.RON, UConst.Bi).Println
+    get_octane_number(flow, UConst.RON, UConst.Bi).Println
   end;
 end.
