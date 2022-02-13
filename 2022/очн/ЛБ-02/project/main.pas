fd95eb7964b4015030dfb544dfbac2126f49291c
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2022-02-13 21:51:59 +0700

    lb-02

--- 2022/очн/ЛБ-02/project/main.pas
+++ 2022/очн/ЛБ-02/project/main.pas
@@ -1,11 +1,23 @@
-﻿uses UFlow;
+﻿uses UFlow, UMixer, UHeatExchanger, USplitter;
 
 
 begin
-  var mass_frac := ArrRandomReal(24, 0, 1);
-  var flow := new Flow(mass_frac, 1000, 273.15);
+  var f1 := new Flow(ArrRandomReal(24, 0, 1), 100, 500);
+  var f2 := new Flow(ArrRandomReal(24, 0, 1), 200, 300);
+  var f3 := new Flow(ArrRandomReal(24, 0, 1), 300, 700);
   
-  flow.average_mol_mass.Println;
-  flow.mole_flow_rate.Println;
-  flow.volume_fractions.Sum.Println
+  var mix1 := new Mixer;
+  var f4 := mix1.calculate(|f1, f2, f3|);
+  
+  f4.mass_flow_rate.Println;
+  f4.temperature.Println;
+  
+  var he1 := new HeatExchanger;
+  var res := he1.calculate(f3, f2);
+  var (f5, f6) := (res[0], res[1]);
+  
+  var spl := new Splitter(|0.333, 0.333, 0.333|);
+  var r := spl.calculate(f5);
+  var (f7, f8, f9) := (r[0], r[1], r[2]);
+  f7.mass_flow_rate.Println
 end.
