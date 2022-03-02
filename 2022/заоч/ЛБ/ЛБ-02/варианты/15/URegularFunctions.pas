e3581a87f3eaae91e629c20159e6d6ab420912d2
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2022-03-02 15:32:16 +0700

    lb-02 zaoch

---
+++ 2022/заоч/ЛБ/ЛБ-02/варианты/15/URegularFunctions.pas
@@ -0,0 +1,111 @@
+﻿unit URegularFunctions;
+
+
+interface
+
+
+function convert_mass_to_mole_fractions(
+  mass_fractions: array of real;
+  mr: array of real): array of real;
+
+function convert_mass_to_volume_fractions(
+  mass_fractions:array of real;
+  density: array of real): array of real;
+
+function get_flow_density(mass_fractions: array of real;
+  density: array of real): real;
+
+function get_average_mol_mass(mass_fractions: array of real;
+  mr: array of real): real;
+
+function get_flow_cp(mass_fractions: array of real;
+  temperature: real;
+  coeffs: array of array of real): real;
+
+function normalize(x: array of real): array of real;
+
+
+implementation
+
+
+function convert_mass_to_mole_fractions(
+  mass_fractions: array of real;
+  mr: array of real): array of real;
+begin
+  result := ArrFill(mass_Fractions.Length, 0.0);
+  var s := 0.0;
+  
+  foreach var i in mass_fractions.Indices do
+    s += mass_fractions[i] / mr[i];
+    
+  foreach var i in mass_Fractions.Indices do
+    result[i] := mass_fractions[i] / mr[i] / s
+end;
+
+
+function convert_mass_to_volume_fractions(
+  mass_fractions: array of real;
+  density: array of real): array of real;
+begin
+  result := ArrFill(mass_fractions.Length, 0.0);
+  var s := 0.0;
+  
+  foreach var i in mass_fractions.Indices do
+    s += mass_fractions[i] / density[i];
+  
+  foreach var i in mass_Fractions.Indices do
+    result[i] := mass_fractions[i] / density[i] / s
+end;
+
+
+function get_flow_density(mass_fractions: array of real;
+  density: array of real): real;
+begin
+  result := 0.0;
+  
+  foreach var i in mass_fractions.Indices do
+    result += mass_fractions[i] / density[i];
+  
+  result := result ** -1
+end;
+
+
+function get_average_mol_mass(mass_fractions: array of real;
+  mr: array of real): real;
+begin
+  result := 0.0;
+  
+  foreach var i in mass_Fractions.Indices do
+    result += mass_fractions[i] / mr[i];
+  
+  result := result ** -1
+end;
+
+
+function get_flow_cp(mass_fractions: array of real;
+  temperature: real;
+  coeffs: array of array of real): real;
+begin
+  result := 0.0;
+  var cp := ArrFill(mass_fractions.Length, 0.0);
+  
+  foreach var i in mass_fractions.Indices do
+    foreach var j in coeffs[i].Indices do
+      cp[i] += (j + 1) * coeffs[i][j] * temperature ** j;
+    
+  foreach var i in mass_fractions.Indices do
+    result += mass_fractions[i] * cp[i]
+end;
+
+
+function normalize(x: array of real): array of real;
+begin
+  result := ArrFill(x.Length, 0.0);
+  var s := x.Sum;
+  
+  foreach var i in x.Indices do
+    result[i] := x[i] / s
+end;
+
+
+end.
---
+++ 2022/заоч/ЛБ/ЛБ-02/варианты/15/USplitter.pas
@@ -0,0 +1,42 @@
+﻿Unit USplitter;
+
+
+interface
+
+
+uses UFlow, URegularFunctions;
+
+
+type
+  Splitter = class
+    ratio: array of real;
+    
+    constructor(ratio: array of real);
+    function calculate(flow_: Flow): array of Flow;
+  end;
+
+
+implementation
+
+
+constructor Splitter.Create(
+  ratio: array of real);
+begin
+  self.ratio := normalize(ratio)
+end;
+
+
+function Splitter.calculate(flow_: Flow): array of Flow;
+begin
+  var mass_flows := ArrFill(self.ratio.Length, 0.0);
+  foreach var i in self.ratio.Indices do
+    mass_flows[i] := self.ratio[i] * flow_.mass_flow_rate;
+  
+  SetLength(result, self.ratio.Length);
+  foreach var i in result.Indices do
+    result[i] := new Flow(
+      flow_.mass_fractions, mass_flows[i], flow_.temperature)
+end;
+
+
+end.
