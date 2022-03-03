4b4bf7370fd7b9d55951315924dfac7c978162f9
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2022-03-03 14:04:36 +0700

    lb-03

---
+++ 2022/заоч/ЛБ/ЛБ-03/Варианты/7/Runge_Kutta.pas
@@ -0,0 +1,58 @@
+﻿unit Runge_Kutta;
+
+
+interface
+
+
+function rk_sum(arr1, arr2: array of real; coef: real): array of real;
+
+function rk(func: function(time: real; c, k: array of real): array of real; 
+  c, k: array of real; start, stop: real;
+  h: real := 0.01): array of array of real;
+
+
+implementation
+
+
+function rk_sum(arr1, arr2: array of real; coef: real): array of real;
+begin
+  result := ArrFill(arr1.Length, 0.0);
+  foreach var i in result.Indices do
+    result[i] := arr1[i] + arr2[i] * coef
+end;
+
+
+function rk(func: function(time: real; c, k: array of real): array of real; 
+  c, k: array of real; start, stop: real;
+  h: real): array of array of real;
+  
+ begin 
+  var time := start;
+  var c_ := copy(c);
+  var k1, k2, k3, k4: array of real;
+  var n := Trunc((stop - start) / h) + 1;
+  
+  SetLength(result, n);
+  foreach var i in result.Indices do
+    SetLength(result[i], c_.Length + 1);
+  
+  foreach var i in result.Indices do
+  begin
+    result[i][0] := time;
+    result[i][1:] := c_;
+    
+    k1 := func(time, c_, k);
+    k2 := func(time + h / 2, rk_sum(c_, k1, h / 2), k);
+    k3 := func(time + h / 2, rk_sum(c_, k2, h / 2), k);
+    k4 := func(time + h, rk_sum(c_, k3, h), k);
+    
+    foreach var j in c_.Indices do
+      c_[j] += h / 6 * (k1[j] + 2 * k2[j] + 2 * k3[j] + k4[j]);
+    
+    time += h
+  end;
+  
+ end;
+
+
+end.
---
+++ 2022/заоч/ЛБ/ЛБ-03/Варианты/7/main.pas
@@ -0,0 +1,48 @@
+﻿uses Runge_Kutta, UGeneticAlgorithm;
+
+
+function kin_scheme(time: real; c, k: array of real): array of real;
+begin
+  result := ArrFill(c.Length, 0.0);
+  result[0] := -k[0] * c[0] * c[1];
+  result[1] := -k[0] * c[0] * c[1] - k[1] * c[2] * c[1];
+  result[2] := k[0] * c[0] * c[1] - k[1] * c[2] * c[1];
+  result[3] := k[1] * c[2] * c[1]
+end;
+
+
+function obj_func(k, act_val: array of real): real;
+begin
+  var res := rk(kin_scheme, |1.0, 1.0, 0.0, 0.0|, k, 0.0, 2.0);
+  var res1d := res[20][1:];
+  foreach var i in range(40, res.High, 20) do
+    res1d := res1d + res[i][1:];
+  
+  foreach var i in act_val.Indices do
+    result += (res1d[i] - act_val[i]) ** 2
+end;
+
+
+begin
+  var act_val: array of real;
+  foreach var (i, val) in ReadLines('var7.txt').Numerate do
+  begin
+    SetLength(act_val, i);
+    act_val[i - 1] := val.ToReal;
+  end;
+  
+  var k := genetic_algorithm(||1e-3, 5.0|, |1e-3, 5.0||, obj_func, act_val)[0];
+  println('Константы скоростей реакций:');
+  println($'k1 = {Round(k[0], 5)}; k2 = {Round(k[1], 5)}');
+  println($'Суммарная ошибка: {k[2]}');
+    
+  println;
+  var res := rk(kin_scheme, |1.0, 1.0, 0.0, 0.0|, k[:^1], 0.0, 2.0);
+  println('Время, с', '  C[0]', '    C[1]', '    C[2]', '    C[3]');
+  foreach var line in res[::20] do
+  begin
+    foreach var i in line.Indices do
+      print($'{line[i]:f4}', ' ');
+    println
+  end
+end.
