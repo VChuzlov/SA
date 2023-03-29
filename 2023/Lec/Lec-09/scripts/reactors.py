1f0944735597800a95e4383c34ad5a566e7686cd
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2023-03-29 12:31:26 +0700

    lec-09

--- 2023/Lec/Lec-09/scripts/reactors.py
+++ 2023/Lec/Lec-09/scripts/reactors.py
@@ -69,19 +69,77 @@ class Bed:
 
 
 class Reactor:
-    """Класс для описания реактора, состоящего из массива полок"""
+    """Класс для описания реактора, представленного в виде массива полок"""
+    def __init__(
+        self,
+        *bed_params: dict
+    ) -> None:
+        self.beds = [Bed(**params) for params in bed_params]
+
+    def calculate(
+        self,
+        kinetic_scheme: callable,
+        feedstock: Flow,
+    ) -> Flow:
+        self.feedstock = feedstock
+        f = self.feedstock
+        
+        for bed in self.beds:
+            f = bed.calculate(kinetic_scheme, f)
+        self.product = f
+        
+        return self.product
+    
+    def performance(self) -> dict:
+        perf = {
+            'Выход изомеризата, % масс.': (
+                self.product.mass_fractions[:-1].sum() 
+                - self.product.mass_fractions[3]
+            ) * 100,
+            'Выход изоалканов, % масс.': (
+                    self.product.mass_fractions[[1, 5, 9, 13]].sum() 
+                    / (1 - self.product.mass_fractions[[3, 16]].sum()) 
+            ) * 100,
+            'Выход iC5, % масс.': (
+                self.product.mass_fractions[1]
+                 / (1 - self.product.mass_fractions[[3, 16]].sum()) 
+            ) * 100,
+            'Выход iC6, % масс.': (
+                self.product.mass_fractions[5]
+                 / (1 - self.product.mass_fractions[[3, 16]].sum()) 
+            ) * 100,
+        }
+        return perf
+
 
 if __name__ == '__main__':
     bed_params = {
         'diameter': .5,
         'height': 1.5,
     }
-    bed = Bed(**bed_params)
+    mf = np.array(
+        [
+            0.344, 0.1075, 0.043, 0., 0.1828, 0.2258, 0.0861, 0.0108,
+            0.,    0.,     0.,    0., 0.,     0.,     0.,     0.,     0.,
+
+        ]
+    )
     f = Flow(
         1000, 
-        conv.normalize(np.random.random(17)), 
+        mf, 
         temperature=273.15,
         pressure=101.325
     )
-    product = bed.calculate(kinetic_scheme, f)
-    print(product.mass_fractions)
+    h2_mf = np.zeros_like(mf)
+    h2_mf[3] = 1.
+    h2 = Flow(
+        mass_flow_rate=100, 
+        mass_fractions=h2_mf, 
+        temperature=273.15, 
+        pressure=101.325
+    )    
+    r = Reactor(bed_params, bed_params, bed_params)
+    product = r.calculate(kinetic_scheme, f)
+    perf = r.performance()
+    for key in perf:
+        print(f'{key}: {perf[key]:.2f}')
