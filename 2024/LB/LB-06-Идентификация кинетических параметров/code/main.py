eb8c73875514985190885fa72fa34a322a61e26f
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2024-03-12 13:55:11 +0700

    Update main.py

--- 2024/LB/LB-06-Идентификация кинетических параметров/code/main.py
+++ 2024/LB/LB-06-Идентификация кинетических параметров/code/main.py
@@ -7,8 +7,34 @@ from typing import Callable
 from kinetic import kinetic_scheme
 
 
-def obj_func() -> float:
-    return ...
+def obj_func(
+        k: np.ndarray, 
+        c: np.ndarray,
+        kinetic_scheme: Callable,
+        time: np.ndarray,
+        c0: np.ndarray,
+) -> float:
+    """Целевая функция, подлежащая минимизации.
+
+    Args:
+        k (np.ndarray): константы скоростей реакций
+        kinetic_scheme (Callable): функция для расчета правых частей дифф. ур-ний
+        c (np.ndarray): концентрации компонентов из исходных данных, [моль/л]
+        time (np.ndarray): значения времени, которым соответствуют концентрации c
+        c0 (np.ndarray): начальные концентрации компонентов, [моль/л]
+
+    Returns:
+        float: _description_
+    """
+    solution = solve_ivp(
+        fun=kinetic_scheme,
+        t_span=(0, time[-1]),
+        y0=c0,
+        dense_output=True,
+        args=(k, )
+    )
+    c_calc = solution.sol(time)
+    return ((c - c_calc[:, 1:].T) ** 2).sum()
 
 
 def calculate_kinetic_constants() -> np.ndarray:
