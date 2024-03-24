d9040a8fa27cab441a47628d35f17855f5b02d9e
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2024-03-24 21:32:13 +0700

    Update main.py

--- 2024/LB/LB-08-ХТС-2/code/main.py
+++ 2024/LB/LB-08-ХТС-2/code/main.py
@@ -48,5 +48,32 @@ def main2() -> None:
     return
 
 
+def main3() -> None:
+    feedstock = flow.Flow(volume_flow_rate=volume_flow_rate,
+                          molar_fractions=molar_fractions)
+    r1 = reactor.Reactor(v)
+    r1.calculate(
+        kinetic_equations=kinetic.equations,
+        feedstock=feedstock, args=(k, ))
+    products = r1.products
+
+    coeff = np.linspace(1.85, 3., 10)
+    v_array = v * coeff
+    benzene = np.zeros_like(coeff)
+    for i, v2 in enumerate(v_array):
+        r2 = reactor.Reactor(v2)
+        r2.calculate(
+            kinetic_equations=kinetic.equations,
+            feedstock=products,
+            args=(k,)
+        )
+        products2 = r2.products
+        benzene[i] = products2.molar_fractions[-2]
+
+    for v2, value in zip(v_array, benzene):
+        print(f'{v2:6.4f} {value:6.4f}')
+    return
+
+
 if __name__ == '__main__':
-    main2()
+    main3()
