b06389a2ce7cd4efcb198658b38e419044085a65
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2024-04-01 19:59:54 +0700

    lb-07

--- 2024/Заочн/ЛБ/ЛБ-07-ХТС/code/main.py
+++ 2024/Заочн/ЛБ/ЛБ-07-ХТС/code/main.py
@@ -3,57 +3,171 @@ from flows import Flow
 from heat_exchanger import HeatExchanger
 from mixer import Mixer
 from splitter import Splitter
+import matplotlib.pyplot as plt
+
+
+def main() -> None:
+    mf1 = np.array(
+        [
+            0.0000,	0.0000,	0.0975,	0.0000,	0.0010,	0.0098,	0.0098,	0.0010,	
+            0.0000,	0.1463,	0.0000,	0.1463,	0.0010,	0.0000,	0.0010,	0.0001,	
+            0.0001,	0.0000,	0.1951,	0.0488,	0.0000,	0.2926,	0.0010,	0.0488,
+        ]
+    )
+    mf8 = np.array(
+        [
+            0,	0,	0,	0,	0.0009,	0.0093,	0.0093,	0.0009,	0,	0.465,	0,	
+            0.2325,	0.0009,	0,	0.0009,	0.0001,	0.0001,	0,	0,	0.2325,	0,	
+            0,	0.0009,	0.0465,
+        ]
+    )
+    flow1 = Flow(
+        mass_flow_rate=1159,
+        mass_fractions=mf1,
+        temperature=300
+    )
+    
+    splitter1 = Splitter()
+    flow2, flow3, flow4 = splitter1.calculate(
+        flow1, .3333, .3333, .3334
+    )
+
+    flow8 = Flow(
+        mass_flow_rate=3000,
+        mass_fractions=mf8,
+        temperature=500
+    )
+    splitter2 = Splitter()
+    flow9, flow10, flow11 = splitter2.calculate(
+        flow8, .3333, .3333, .3334
+    )
+
+    he1 = HeatExchanger(
+        d_in=.15,
+        d_out=.4,
+        length=3.
+    )
+    flow12, flow5 = he1.calculate(
+        hot=flow9,
+        cold=flow2
+    )
+    he2 = HeatExchanger(
+        d_in=.15,
+        d_out=.4,
+        length=3.
+    )
+    flow14, flow6 = he2.calculate(
+        hot=flow11,
+        cold=flow3
+    )
+    he3 = HeatExchanger(
+        d_in=.15,
+        d_out=.4,
+        length=3.
+    )
+    flow13, flow7 = he3.calculate(
+        hot=flow10,
+        cold=flow4
+    )
+
+    mixer1 = Mixer()
+    flow15 = mixer1.mix(flow12, flow13, flow14)
+
+    mixer2 = Mixer()
+    flow16 = mixer2.mix(flow5, flow6, flow7)
+
+    print('====Flow15====')
+    print(f'Массовый расход: {flow15.mass_flow_rate:>8.2f}')
+    print(f'Температура: {flow15.temperature:>8.2f}')
+    print(f'Состав {flow15.mass_fractions}')
+
+    print('====Flow16====')
+    print(f'Массовый расход: {flow16.mass_flow_rate:>8.2f}')
+    print(f'Температура: {flow16.temperature:>8.2f}')
+    print(f'Состав {flow16.mass_fractions}')
+
+    return
+
+
+def case_study(temperature: float) -> float:
+    mf1 = np.array(
+        [
+            0.0000,	0.0000,	0.0975,	0.0000,	0.0010,	0.0098,	0.0098,	0.0010,	
+            0.0000,	0.1463,	0.0000,	0.1463,	0.0010,	0.0000,	0.0010,	0.0001,	
+            0.0001,	0.0000,	0.1951,	0.0488,	0.0000,	0.2926,	0.0010,	0.0488,
+        ]
+    )
+    mf8 = np.array(
+        [
+            0,	0,	0,	0,	0.0009,	0.0093,	0.0093,	0.0009,	0,	0.465,	0,	
+            0.2325,	0.0009,	0,	0.0009,	0.0001,	0.0001,	0,	0,	0.2325,	0,	
+            0,	0.0009,	0.0465,
+        ]
+    )
+    flow1 = Flow(
+        mass_flow_rate=1159,
+        mass_fractions=mf1,
+        temperature=temperature
+    )
+    
+    splitter1 = Splitter()
+    flow2, flow3, flow4 = splitter1.calculate(
+        flow1, .3333, .3333, .3334
+    )
+
+    flow8 = Flow(
+        mass_flow_rate=3000,
+        mass_fractions=mf8,
+        temperature=500
+    )
+    splitter2 = Splitter()
+    flow9, flow10, flow11 = splitter2.calculate(
+        flow8, .3333, .3333, .3334
+    )
+
+    he1 = HeatExchanger(
+        d_in=.15,
+        d_out=.4,
+        length=3.
+    )
+    _, flow5 = he1.calculate(
+        hot=flow9,
+        cold=flow2
+    )
+    he2 = HeatExchanger(
+        d_in=.15,
+        d_out=.4,
+        length=3.
+    )
+    _, flow6 = he2.calculate(
+        hot=flow11,
+        cold=flow3
+    )
+    he3 = HeatExchanger(
+        d_in=.15,
+        d_out=.4,
+        length=3.
+    )
+    _, flow7 = he3.calculate(
+        hot=flow10,
+        cold=flow4
+    )
+
+    mixer2 = Mixer()
+    flow16 = mixer2.mix(flow5, flow6, flow7)
+
+    return flow16.temperature - flow1.temperature
 
 
 if __name__ == '__main__':
-    f1_mf = np.array(
-        [
-            0, 0, 0.0552, 0, 0.0006, 0.0055, 0.0055, 0.0006, 0,
-            0.0828, 0, 0.1132, 0.0006, 0, 0.0006, 0.0001, 0.0001,
-            0, 0.1699, 0.0276, 0, 0.5096, 0.0006, 0.0276,
-        ]
-    )
-    f2_mf = np.array(
-        [
-            0, 0, 0.0975, 0, 0.001, 0.0098, 0.0098, 0.001, 0,
-            0.1463, 0, 0.1463, 0.001, 0, 0.001, 0.0001, 0.0001,
-            0, 0.1951, 0.0488, 0, 0.2926, 0.001, 0.0488,
-        ]
-    )
-    f3_mf = np.array(
-        [
-            0, 0, 0.0864, 0, 0.0006, 0.0018, 0.0143, 0.0008, 0,
-            0.0516, 0, 0.2079, 0.0002, 0, 0.0013, 0.0001, 0.0001,
-            0, 0.2745, 0.0558, 0, 0.2993, 0.0012, 0.0042,
-        ]
-    )
-    f8_mf = np.array(
-        [
-            0,	0,	0,	0,	0.0009,	0.0093,	0.0093,	0.0009,
-            0,	0.465,	0,	0.2325,	0.0009,	0,	0.0009,	0.0001,
-            0.0001,	0,	0,	0.2325,	0,	0,	0.0009,	0.0465,
-        ]
-    )
-
-    # Создание потоков
-    flow1 = Flow(mass_flow_rate=1130, mass_fractions=f1_mf, temperature=300)
-    flow2 = Flow(mass_flow_rate=2000, mass_fractions=f2_mf, temperature=300)
-    flow3 = Flow(mass_flow_rate=1700, mass_fractions=f3_mf, temperature=268)
-    flow8 = Flow(mass_flow_rate=2000, mass_fractions=f8_mf, temperature=500)
-
-    # Миксер
-    mixer = Mixer()
-    flow4 = mixer.mix(flow1, flow2, flow3)
-
-    # Теплообменный аппарат
-    heat_exchanger = HeatExchanger()
-    flow9, flow5 = heat_exchanger.calculate(flow8, flow4)
-
-    # Делитель
-    splitter = Splitter()
-    flow6, flow7 = splitter.calculate(flow5, .5, .5)
-
-    print(flow6.temperature, flow6.mass_flow_rate)
-
-
-
+    main()
+    t_array = np.arange(300, 460, 10)
+    res = np.zeros_like(t_array)
+    
+    for i, t in enumerate(t_array):
+        res[i] = case_study(t)
+    
+    plt.plot(t_array, res)
+    plt.tight_layout()
+    plt.show()
+    
