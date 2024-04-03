1d24a4943e0b3b3ed60303bb4b8952fa55f9dc0f
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2024-04-03 22:36:08 +0700

    Create kinetic.py

---
+++ 2024/Заочн/ЛБ/ЛБ-08-Идентификация кинетических параметров/code/kinetic.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+
+
+def kinetic_scheme(
+        time: float,
+        c: np.ndarray,
+        k: np.ndarray,
+        stoich_matrix: np.ndarray
+) -> np.ndarray:
+    """Реализация расчета правых частей системы дифференциальных уравнений,
+    описывающих изменение концентрации компонентов, 
+    участвующих в химических превращениях.
+
+    Args:
+        time (float): времени из интервала, на котором наблюдается процесс 
+        c (np.ndarray): концентрации компонентов, [моль/л]
+        k (np.ndarray): константы скорости реакций, входящих в схему
+        stoich_matrix (np.ndarray): матрица стехиометрических коэффициентов
+
+    Returns:
+        np.ndarray: значения правых частей уравнений сис-мы дифф. ур-ний
+    """
+
+    mask = stoich_matrix < 0  # Выбираем элементы с отрицательными стехим. коэфф-тами
+    p = (c ** -(stoich_matrix * mask)).prod(axis=1)  # Произвед. конц-ий реагентов
+    reaction_rates = p * k  # массив скоростей реакций
+    
+    right_parts = (
+        (stoich_matrix.T * reaction_rates).sum(axis=1)
+    )
+    
+    return right_parts
+
+
+if __name__ == '__main__':
+    pass
